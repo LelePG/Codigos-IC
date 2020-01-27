ac00237154e81fbba4799abe94d9c2d866542238
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2020-01-26 22:03:02 -0300

    Exemplos de código para testes do algoritmo

--- Exemplos/Uma_Operacao.java
+++ Exemplos/Uma_Operacao.java
@@ -1,7 +1,8 @@
-public class Atomic{
-	public static void main(String[] args){
-	Account ac1 = 1;
-	atomic{
-		ac1.deposit(10);
+public class Atomic {
+	public static void main(String[] args) {
+		Account ac1 = 1;
+		atomic {
+			ac1.deposit(10);
+		}
 	}
 }
