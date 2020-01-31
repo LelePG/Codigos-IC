1b87c4566a6f9eaf39abfd443f4173009a07584d
Letícia Pegoraro Garcez <lpgarcez@inf.ufpel.edu.br>
2020-01-30 21:28:16 -0300

    Exemplos atualizados

---
+++ Exemplos/Repeticoes.java
@@ -0,0 +1,21 @@
+public class Atomic {
+    public static void main(String[] args) {
+        Account ac1 = 1;
+        int count;
+        System.out.println("oi");
+        count.lenght();//código aleatório
+        for(count = 1; count < 30; count++);//código aleatório
+        atomic {
+            a1.deposit(10);
+            System.out.println("oi2");//aqui dá problema.
+            if(count==40){//if
+                ac1.withdraw(p.getlenght());
+                while(count!=10){//while dentro do if
+                    count --;
+                    ac1.deposit(1);
+                }
+            }
+        }
+        System.out.println("findou");//código aleatório
+    }
+}
--- Exemplos/Uma_Operacao.java
+++ Exemplos/Uma_Operacao.java
@@ -2,7 +2,7 @@ public class Atomic {
 	public static void main(String[] args) {
 		Account ac1 = 1;
 		atomic {
-			ac1.deposit(10);
+			a1.deposit(10);
 		}
 	}
 }
